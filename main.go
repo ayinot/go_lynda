6d7ea442f812e377be621e21e57ba8a013cc7aff
Toniya <toniya.sundaram@gmail.com>
2019-05-24 10:52:09 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// //Capper implements io.writer and turns everything to uppsercase
+// type Capper struct {
+// 	wtr io.Writer
+// }
+
+// func (c *Capper) Write(p []byte) (n int, err error) {
+// 	diff := byte('a' - 'A')
+// 	out := make([]byte, len(p))
+// 	for i, c := range p {
+// 		if c >= 'a' && c <= 'z' {
+// 			c -= diff 
+// 		}
+// 		out[i] = c
+// 	}
+// 	return c.wtr.Write(out)
+// }
+
+// func main() {
+// 	c := &Capper{os.Stdout}
+// 	fmt.Fprintln(c, "Hello there")
+// }
